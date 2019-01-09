1897d0a33d88a30a53a88b8af5f27b9d8f42b75a
stephenautomatik <sdickens@automatik.us>
2019-01-09 08:18:22 -0700

    locations added

--- src/app/main/location/location.component.ts
+++ src/app/main/location/location.component.ts
@@ -16,7 +16,7 @@ export class LocationData {
   venue: string;
   venueImage: string;
   venueSubtitle: string;
-  venueInfo: string;
+  venueDates: string;
 }
 
 @Component({
@@ -70,62 +70,62 @@ export class LocationComponent implements OnInit {
 
     this.location.title = city;
 
-    if (city === 'Irving, TX') {
-      this.location.id = 0;
-      this.location.subtitle = 'The heart of the Dallas / Fort Worth Metroplex';
-      this.location.image = '../../../assets/images/location-irving.jpg';
-      this.location.venue = 'Irving Venue Name';
-      this.location.venueImage = '../../../assets/images/location-irving.jpg';
-      this.location.venueSubtitle = 'Irving Venue Subtitle';
-      this.location.venueInfo = 'Irving Venue Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt, eveniet. Cupiditate repellat saepe hic vel tempora ducimus quae illo! Quia dignissimos id dolores ut placeat voluptatibus sed aspernatur totam laborum!';
-    } else if (city === 'Miami, FL') {
+    if (city === 'Miami, FL') {
       this.location.id = 1;
       this.location.subtitle = 'The Magic City';
       this.location.image = '../../../assets/images/location-miami.jpg';
-      this.location.venue = 'Miami Venue Name';
-      this.location.venueImage = '../../../assets/images/location-miami.jpg';
-      this.location.venueSubtitle = 'Miami Venue Subtitle';
-      this.location.venueInfo = 'Miami Venue Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt, eveniet. Cupiditate repellat saepe hic vel tempora ducimus quae illo! Quia dignissimos id dolores ut placeat voluptatibus sed aspernatur totam laborum!';
-    } else if (city === 'Rosemont, IL') {
+      this.location.venue = 'Marriott Miami Airport';
+      this.location.venueImage = '../../../assets/images/venue-miami.jpg';
+      this.location.venueSubtitle = '1201 NW LeJeune Rd Building A\nMiami, FL 33126';
+      this.location.venueDates = 'Training waves at this location will be held on March 4, 5 & 6. If one of these dates does not appear as an option while you are registering, it is likely full, so please select a different date.';
+    } else if (city === 'Irving, TX') {
       this.location.id = 2;
+      this.location.subtitle = 'The heart of the Dallas / Fort Worth Metroplex';
+      this.location.image = '../../../assets/images/location-irving.jpg';
+      this.location.venue = 'Doubletree by Hilton DFW North';
+      this.location.venueImage = '../../../assets/images/venue-irving.jpg';
+      this.location.venueSubtitle = '4441 W John Carpenter Freeway\nIrving, TX 75063';
+      this.location.venueDates = 'Training waves at this location will be held on March 6 & 7. If one of these dates does not appear as an option while you are registering, it is likely full, so please select a different date.';
+    } else if (city === 'Rosemont, IL') {
+      this.location.id = 3;
       this.location.subtitle = 'You Can\'t Miss It';
       this.location.image = '../../../assets/images/location-rosemont.jpg';
-      this.location.venue = 'Rosemont Venue Name';
-      this.location.venueImage = '../../../assets/images/location-rosemont.jpg';
-      this.location.venueSubtitle = 'Rosemont Venue Subtitle';
-      this.location.venueInfo = 'Rosemont Venue Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt, eveniet. Cupiditate repellat saepe hic vel tempora ducimus quae illo! Quia dignissimos id dolores ut placeat voluptatibus sed aspernatur totam laborum!';
+      this.location.venue = 'Sheraton Suites O’Hare Airport';
+      this.location.venueImage = '../../../assets/images/venue-rosemont.jpg';
+      this.location.venueSubtitle = '6501 Mannheim Rd\nRosemont, IL 60018';
+      this.location.venueDates = 'Training waves at this location will be held on March 13 & 14. If one of these dates does not appear as an option while you are registering, it is likely full, so please select a different date.';
     } else if (city === 'Santa Ana, CA') {
-      this.location.id = 3;
+      this.location.id = 4;
       this.location.subtitle = 'The Spirit of Change';
       this.location.image = '../../../assets/images/location-santa-ana.jpg';
-      this.location.venue = 'Santa Ana Venue Name';
-      this.location.venueImage = '../../../assets/images/location-santa-ana.jpg';
-      this.location.venueSubtitle = 'Santa Ana Venue Subtitle';
-      this.location.venueInfo = 'Santa Ana Venue Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt, eveniet. Cupiditate repellat saepe hic vel tempora ducimus quae illo! Quia dignissimos id dolores ut placeat voluptatibus sed aspernatur totam laborum!';
+      this.location.venue = 'Doubletree by Hilton Santa Ana – Orange County Airport';
+      this.location.venueImage = '../../../assets/images/venue-santa-ana.jpg';
+      this.location.venueSubtitle = '201 East MacArthur Blvd\nSanta Ana, CA 92707';
+      this.location.venueDates = 'Training waves at this location will be held on March 13, 14, and 15. If one of these dates does not appear as an option while you are registering, it is likely full, so please select a different date.';
     } else if (city === 'Oakland, CA') {
-      this.location.id = 4;
+      this.location.id = 5;
       this.location.subtitle = 'Bright Side of the Bay';
       this.location.image = '../../../assets/images/location-oakland.jpg';
-      this.location.venue = 'Oakland Venue Name';
-      this.location.venueImage = '../../../assets/images/location-oakland.jpg';
-      this.location.venueSubtitle = 'Oakland Venue Subtitle';
-      this.location.venueInfo = 'Oakland Venue Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt, eveniet. Cupiditate repellat saepe hic vel tempora ducimus quae illo! Quia dignissimos id dolores ut placeat voluptatibus sed aspernatur totam laborum!';
+      this.location.venue = 'Hilton Oakland Airport';
+      this.location.venueImage = '../../../assets/images/venue-oakland.jpg';
+      this.location.venueSubtitle = '1 Hegenberger Rd\nOakland, CA 94621';
+      this.location.venueDates = 'Training waves at this location will be held on March 18, 19, and 20. If one of these dates does not appear as an option while you are registering, it is likely full, so please select a different date.';
     } else if (city === 'Mahwah, NJ') {
-      this.location.id = 5;
+      this.location.id = 6;
       this.location.subtitle = 'Place Where Paths Meet';
       this.location.image = '../../../assets/images/location-mahwah.jpg';
-      this.location.venue = 'Mahwah Venue Name';
-      this.location.venueImage = '../../../assets/images/location-mahwah.jpg';
-      this.location.venueSubtitle = 'Mahwah Venue Subtitle';
-      this.location.venueInfo = 'Mahwah Venue Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt, eveniet. Cupiditate repellat saepe hic vel tempora ducimus quae illo! Quia dignissimos id dolores ut placeat voluptatibus sed aspernatur totam laborum!';
+      this.location.venue = 'Jaguar Land Rover North American Headquarters';
+      this.location.venueImage = '../../../assets/images/venue-mahwah.jpg';
+      this.location.venueSubtitle = '100 Jaguar Land Rover Way\nMahwah, NJ 07495';
+      this.location.venueDates = 'Training waves at this location will be held on March 18, 19, 20 & 21. If one of these dates does not appear as an option while you are registering, it is likely full, so please select a different date.';
     } else if (city === 'Toronto, ON') {
-      this.location.id = 6;
+      this.location.id = 7;
       this.location.subtitle = 'Hogtown';
       this.location.image = '../../../assets/images/location-toronto.jpg';
-      this.location.venue = 'Toronto Venue Name';
-      this.location.venueImage = '../../../assets/images/location-toronto.jpg';
-      this.location.venueSubtitle = 'Toronto Venue Subtitle';
-      this.location.venueInfo = 'Toronto Venue Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt, eveniet. Cupiditate repellat saepe hic vel tempora ducimus quae illo! Quia dignissimos id dolores ut placeat voluptatibus sed aspernatur totam laborum!';
+      this.location.venue = 'Hilton Garden Inn Toronto Airport';
+      this.location.venueImage = '../../../assets/images/venue-toronto.jpg';
+      this.location.venueSubtitle = '3311 Caroga Dr\nMississauga, ON L4V 1A3, Canada';
+      this.location.venueDates = 'Training waves at this location will be held on April 2 & 3. If one of these dates does not appear as an option while you are registering, it is likely full, so please select a different date.';
     }
   }
 
